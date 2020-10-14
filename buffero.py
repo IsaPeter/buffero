e521ee24bed64bc773001e992c417bdf49bcab43
isap <venom@kali.local>
2020-10-14 16:43:49 +0200

    comments

--- buffero.py
+++ buffero.py
@@ -8,24 +8,33 @@ sub_esp_10= "\x83\xec\x10"  # sub esp,0x10 (drop esp back to 16 bytes)
 
 # These are useful methods for testing purposes while develop an exploit
 def encode(string):
-    return string.encode(encoding='latin-1')
+    return string.encode(encoding='latin-1')    # Encoding the given string to Latin-1 type byze array
 def decode(bytearr):
-    return bytearr.decode(encoding='latin-1')
+    return bytearr.decode(encoding='latin-1')   # Decoding the given byte array to Latin-1 encoded string 
 def generate_bctest(badchars=['0x00']):
-    bctest = ""
-    for i in range(0x00,0xFF+1):
+    """
+    This function will generate character array for bad char identification.
+    """
+    
+    bctest = ""     # the returnes string
+    for i in range(0x00,0xFF+1):       # python range 0 - 255 (+1 is need for 255)
         if i not in badchars:
-            bctest += chr(i)
-    with open('bctest.bin','wb') as f:
-        f.write(encode(bctest))
-    return bctest
+            bctest += chr(i)            # Add character to string ii it is not a known bad char
+    with open('bctest.bin','wb') as f:  
+        f.write(encode(bctest))         # write the result into a binary (required for mona)
+    return bctest                       # return the resulted string
 def generate_pattern(plength):
-    pattern = ""
-    i = 0 # numbers
-    j = 0 # lower alphabet
-    k = 0 # Uppel Letters
+    """
+    This function will generate unique cyclic string for EIP identification.
+    The result is the same of msf-pattern_create. The result works with msf-pattern_offset.
+    """
+    
+    pattern = ""        # The result pattern string
+    i = 0               # counter for numbers
+    j = 0               # counter for lower alphabet
+    k = 0               # counter for Uppel Letters
     while len(pattern)<plength:
-        pattern += _get_next_pattern_string(i,j,k)
+        pattern += _get_next_pattern_string(i,j,k)  # Generate the next pattern element
         i += 1
         if i == 10:
             i = 0
@@ -34,22 +43,27 @@ def generate_pattern(plength):
                 j= 0
                 k+=1
     if len(pattern)>plength:
-        pattern = pattern[:plength]
-        return pattern
+        pattern = pattern[:plength] # Split the pattern to the correct size
+        return pattern          # return pattern
     elif len(pattern)==plength:
         return pattern
 
 def _get_next_pattern_string(i,j,k):
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    return alphabet[k].upper()+alphabet[j]+str(i)
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] # alphabet
+    return alphabet[k].upper()+alphabet[j]+str(i)   # generate pattern element from the given parameters, and return it.
 
 
 def get_pattern_offset(length, EIP):
-    ba = bytearray.fromhex(EIP)
-    ba.reverse()
-    eip = ba.decode(encoding='latin-1')
-    pattern = generate_pattern(length)
-    a = pattern.find(eip)
+    """
+    This modul will determine the offset of the given EPI stored value.
+    The function is fully compatible with msf-pattern_create. The functionality is similar to msf-pattern_offset
+    """
+    
+    ba = bytearray.fromhex(EIP) # create a byte array from EIP stored HEX code
+    ba.reverse()                # reverse if because Little Endian
+    eip = ba.decode(encoding='latin-1') # Get the result string from decode
+    pattern = generate_pattern(length)  # generate a given length of pattern
+    a = pattern.find(eip)               # find the given EIP value in the generated pattern
     if a > 0:
         print(f"Offset found near: {str(a)}")
     else:
@@ -57,8 +71,14 @@ def get_pattern_offset(length, EIP):
     return a
     
     
-# Pattern 2000:
-# Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba6Ba7Ba8Ba9Bb0Bb1Bb2Bb3Bb4Bb5Bb6Bb7Bb8Bb9Bc0Bc1Bc2Bc3Bc4Bc5Bc6Bc7Bc8Bc9Bd0Bd1Bd2Bd3Bd4Bd5Bd6Bd7Bd8Bd9Be0Be1Be2Be3Be4Be5Be6Be7Be8Be9Bf0Bf1Bf2Bf3Bf4Bf5Bf6Bf7Bf8Bf9Bg0Bg1Bg2Bg3Bg4Bg5Bg6Bg7Bg8Bg9Bh0Bh1Bh2Bh3Bh4Bh5Bh6Bh7Bh8Bh9Bi0Bi1Bi2Bi3Bi4Bi5Bi6Bi7Bi8Bi9Bj0Bj1Bj2Bj3Bj4Bj5Bj6Bj7Bj8Bj9Bk0Bk1Bk2Bk3Bk4Bk5Bk6Bk7Bk8Bk9Bl0Bl1Bl2Bl3Bl4Bl5Bl6Bl7Bl8Bl9Bm0Bm1Bm2Bm3Bm4Bm5Bm6Bm7Bm8Bm9Bn0Bn1Bn2Bn3Bn4Bn5Bn6Bn7Bn8Bn9Bo0Bo1Bo2Bo3Bo4Bo5Bo6Bo7Bo8Bo9Bp0Bp1Bp2Bp3Bp4Bp5Bp6Bp7Bp8Bp9Bq0Bq1Bq2Bq3Bq4Bq5Bq6Bq7Bq8Bq9Br0Br1Br2Br3Br4Br5Br6Br7Br8Br9Bs0Bs1Bs2Bs3Bs4Bs5Bs6Bs7Bs8Bs9Bt0Bt1Bt2Bt3Bt4Bt5Bt6Bt7Bt8Bt9Bu0Bu1Bu2Bu3Bu4Bu5Bu6Bu7Bu8Bu9Bv0Bv1Bv2Bv3Bv4Bv5Bv6Bv7Bv8Bv9Bw0Bw1Bw2Bw3Bw4Bw5Bw6Bw7Bw8Bw9Bx0Bx1Bx2Bx3Bx4Bx5Bx6Bx7Bx8Bx9By0By1By2By3By4By5By6By7By8By9Bz0Bz1Bz2Bz3Bz4Bz5Bz6Bz7Bz8Bz9Ca0Ca1Ca2Ca3Ca4Ca5Ca6Ca7Ca8Ca9Cb0Cb1Cb2Cb3Cb4Cb5Cb6Cb7Cb8Cb9Cc0Cc1Cc2Cc3Cc4Cc5Cc6Cc7Cc8Cc9Cd0Cd1Cd2Cd3Cd4Cd5Cd6Cd7Cd8Cd9Ce0Ce1Ce2Ce3Ce4Ce5Ce6Ce7Ce8Ce9Cf0Cf1Cf2Cf3Cf4Cf5Cf6Cf7Cf8Cf9Cg0Cg1Cg2Cg3Cg4Cg5Cg6Cg7Cg8Cg9Ch0Ch1Ch2Ch3Ch4Ch5Ch6Ch7Ch8Ch9Ci0Ci1Ci2Ci3Ci4Ci5Ci6Ci7Ci8Ci9Cj0Cj1Cj2Cj3Cj4Cj5Cj6Cj7Cj8Cj9Ck0Ck1Ck2Ck3Ck4Ck5Ck6Ck7Ck8Ck9Cl0Cl1Cl2Cl3Cl4Cl5Cl6Cl7Cl8Cl9Cm0Cm1Cm2Cm3Cm4Cm5Cm6Cm7Cm8Cm9Cn0Cn1Cn2Cn3Cn4Cn5Cn6Cn7Cn8Cn9Co0Co1Co2Co3Co4Co5Co
+def generate_nop_sled(length):
+    if length > 0:
+        return '\x90'*length    # generate a spaecified length NOP sled
+    else:
+        return ''
 
-# get_pattern_offset(2000,'42376E42')
-# 0x42376E42 = \x42\x6e\x37\x43 = Bn7B
+
+
+
+p = generate_pattern(7000)
+print(p)
